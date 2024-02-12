01d761d3a966000d9ee39ba8f1bb9b10bd9f103a
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-02-12 12:00:00 +0100

    feat: connection to mongo database

---
+++ TaskFlow/TaskFlow/Models/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskFlow.Models
+{
+
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CollectionName { get; set; } = String.Empty;
+        public string ConnectionString { get; set; } = String.Empty;
+        public string DatabaseName { get; set; } = String.Empty;
+    }
+}
